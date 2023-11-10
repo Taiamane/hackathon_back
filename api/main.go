94564e808a4eb02b75596429548063defdf24e8e
Taiamane <fujiura-taishu749@g.ecc.u-tokyo.ac.jp>
2023-11-10 09:35:47 +0900

    雛形？

--- api/main.go
+++ api/main.go
@@ -14,6 +14,7 @@ import (
 	"github.com/oklog/ulid"
 
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/gorilla/mux"
 )
 
 type ItemData struct {
@@ -57,67 +58,52 @@ func handler(w http.ResponseWriter, r *http.Request) {
 
 	switch r.Method {
 	case http.MethodGet:
-		curriculum := r.URL.Query().Get("curriculum")
-		if curriculum == "" {
-			rows, err := db.Query("SELECT CATEGORY ,CURRICULUM,TITLE,LINK,SUMMARY,MADE_DATE,UPDATED_DAY FROM ITEMS ")
-			if err != nil {
-				log.Printf("fail: db.Query, %v\n", err)
-				w.WriteHeader(http.StatusInternalServerError)
-				return
-			}
-			itemsdata := make([]ItemData, 0)
-			for rows.Next() {
-				var u ItemData
-				if err := rows.Scan(&u.Category, &u.Curriculum, &u.Title, &u.Link, &u.Summary, &u.Made_day, &u.Updated_day); err != nil {
-					log.Printf("fail: rows.Scan, %v\n", err)
+		sortKey := r.URL.Query().Get("sort")
+		order := "made_day DESC" // デフォルトのソート順（作成日時の降順）
 
-					if err := rows.Close(); err != nil { // 500を返して終了するが、その前にrowsのClose処理が必要
-						log.Printf("fail: rows.Close(), %v\n", err)
-					}
-					w.WriteHeader(http.StatusInternalServerError)
-					return
-				}
-				itemsdata = append(itemsdata, u)
-			}
-
-			bytes, err := json.Marshal(itemsdata)
-			if err != nil {
-				log.Printf("fail: json.Marshal, %v\n", err)
-				w.WriteHeader(http.StatusInternalServerError)
-				return
-			}
-			w.Header().Set("Content-Type", "application/json")
-			w.Write(bytes)
-		} else {
-			rows, err := db.Query("SELECT category,curriculum,title,link,summary,made_day,updated_day FROM ITEMS WHERE curriculum = ?", curriculum)
-			if err != nil {
-				log.Printf("fail:db.Query,%v\n", err)
-				w.WriteHeader(http.StatusInternalServerError)
-				return
-			}
-			itemsdata := make([]ItemData, 0)
-			for rows.Next() {
-				var u ItemData
-				if err := rows.Scan(u.Category, &u.Curriculum, &u.Title, &u.Link, &u.Summary, &u.Made_day, &u.Updated_day); err != nil {
-					log.Printf("fail:rows.Scan,%v\n", err)
-					if err := rows.Close(); err != nil {
-						log.Printf("fail:rows.Close(),%v\n", err)
-					}
-					w.WriteHeader(http.StatusInternalServerError)
-					return
-				}
-				itemsdata = append(itemsdata, u)
-			}
-
-			bytes, err := json.Marshal(itemsdata)
-			if err != nil {
-				log.Printf("fail: json.Marshal, %v\n", err)
-				w.WriteHeader(http.StatusInternalServerError)
-				return
-			}
-			w.Header().Set("Content-Type", "application/json")
-			w.Write(bytes)
+		if sortKey != "" {
+			order = fmt.Sprintf("%s %s", sortKey, r.URL.Query().Get("order"))
 		}
+
+		curriculum := r.URL.Query().Get("curriculum")
+		query := "SELECT category, curriculum, title, link, summary, made_day, updated_day FROM items"
+
+		if curriculum != "" {
+			query += " WHERE curriculum = ?"
+		}
+
+		query += fmt.Sprintf(" ORDER BY %s", order)
+
+		rows, err := db.Query(query, curriculum)
+		if err != nil {
+			log.Printf("fail: db.Query, %v\n", err)
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+
+		itemsData := make([]ItemData, 0)
+		for rows.Next() {
+			var u ItemData
+			if err := rows.Scan(&u.Category, &u.Curriculum, &u.Title, &u.Link, &u.Summary, &u.Made_day, &u.Updated_day); err != nil {
+				log.Printf("fail: rows.Scan, %v\n", err)
+
+				if err := rows.Close(); err != nil { // 500を返して終了するが、その前にrowsのClose処理が必要
+					log.Printf("fail: rows.Close(), %v\n", err)
+				}
+				w.WriteHeader(http.StatusInternalServerError)
+				return
+			}
+			itemsData = append(itemsData, u)
+		}
+		bytes, err := json.Marshal(itemsData)
+		if err != nil {
+			log.Printf("fail: json.Marshal, %v\n", err)
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		w.Write(bytes)
 
 	case http.MethodPost:
 		time := time.Now()
@@ -170,8 +156,88 @@ func handler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 }
+
+func detailHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000") // 後でVercelのURLに変更
+	w.Header().Set("Access-Control-Allow-Methods", "GET")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+
+	vars := mux.Vars(r)
+	id := vars["id"]
+
+	row := db.QueryRow("SELECT CATEGORY, CURRICULUM, TITLE, LINK, SUMMARY, MADE_DAY, UPDATED_DAY FROM ITEMS WHERE MADE_DAY = ?", id)
+
+	var item ItemData
+	err := row.Scan(&item.Category, &item.Curriculum, &item.Title, &item.Link, &item.Summary, &item.Made_day, &item.Updated_day)
+	if err != nil {
+		log.Printf("fail: db.QueryRow, %v\n", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	bytes, err := json.Marshal(item)
+	if err != nil {
+		log.Printf("fail: json.Marshal, %v\n", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(bytes)
+}
+
+func editHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000") // 後でVercelのURLに変更
+	w.Header().Set("Access-Control-Allow-Methods", "PUT")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+
+	vars := mux.Vars(r)
+	id := vars["id"]
+
+	var requestData ItemData
+	err := json.NewDecoder(r.Body).Decode(&requestData)
+	if err != nil {
+		log.Printf("fail: json.NewDecoder, %v\n", err)
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	_, err = db.Exec("UPDATE ITEMS SET CATEGORY=?, CURRICULUM=?, TITLE=?, LINK=?, SUMMARY=?, UPDATED_DAY=? WHERE MADE_DAY=?", requestData.Category, requestData.Curriculum, requestData.Title, requestData.Link, requestData.Summary, time.Now(), id)
+	if err != nil {
+		log.Printf("fail: db.Exec, %v\n", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusNoContent)
+}
+
+func deleteHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000") // 後でVercelのURLに変更
+	w.Header().Set("Access-Control-Allow-Methods", "DELETE")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+
+	vars := mux.Vars(r)
+	id := vars["id"]
+
+	_, err := db.Exec("DELETE FROM ITEMS WHERE MADE_DAY=?", id)
+	if err != nil {
+		log.Printf("fail: db.Exec, %v\n", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusNoContent)
+}
+
 func main() {
-	http.HandleFunc("/user", handler)
+	r := mux.NewRouter()
+
+	// ハンドラの登録
+	r.HandleFunc("/items", handler).Methods("GET", "POST")
+	r.HandleFunc("/items/{id}", detailHandler).Methods("GET")
+	r.HandleFunc("/items/{id}", editHandler).Methods("PUT")
+	r.HandleFunc("/items/{id}", deleteHandler).Methods("DELETE")
 
 	closeDBWithSysCall()
 
