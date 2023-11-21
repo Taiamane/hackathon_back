8063f880d3501b20babe7dbe0b328efc14e2948c
Taiamane <fujiura-taishu749@g.ecc.u-tokyo.ac.jp>
2023-11-21 21:00:27 +0900

    headerを戻してみた

--- api/main.go
+++ api/main.go
@@ -61,11 +61,11 @@ func init() {
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	// w.Header().Set("Access-Control-Allow-Origin", "*") //後でvercelのURLに書き換える
-	// w.Header().Set("Access-Control-Allow-Methods", "GET, POST,PUT,DELETE,OPTIONS")
-	// w.Header().Set("Access-Control-Allow-Credentials", "true")
-	// w.Header().Set("Access-Control-Allow-Headers", "*")
-	// w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Access-Control-Allow-Origin", "*") //後でvercelのURLに書き換える
+	w.Header().Set("Access-Control-Allow-Methods", "GET, POST,PUT,DELETE,OPTIONS")
+	w.Header().Set("Access-Control-Allow-Credentials", "true")
+	w.Header().Set("Access-Control-Allow-Headers", "*")
+	w.Header().Set("Content-Type", "application/json")
 
 	if r.Method == http.MethodOptions {
 		w.WriteHeader(http.StatusOK)
